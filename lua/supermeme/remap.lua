baaef5c8c9d9df5e223fe842dcf08e23207cf466
repusemem <enderman2408@gmail.com>
2026-06-29 10:49:41 +0700

    remap

--- lua/supermeme/remap.lua
+++ lua/supermeme/remap.lua
@@ -2,6 +2,19 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>")
+
+vim.keymap.set("n", "<leader>sv", "<cmd>vsplit<CR>", { desc = "Vertical split" })
+vim.keymap.set("n", "<leader>sh", "<cmd>split<CR>", { desc = "Horizontal split" })
+
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close split" })
+
+vim.keymap.set("n", "<leader>so", "<cmd>only<CR>", { desc = "Only current split" })
+
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +5<CR>", { desc = "Increase height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -5<CR>", { desc = "Decrease height" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +5<CR>", { desc = "Increase width" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -5<CR>", { desc = "Decrease width" })
+
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
