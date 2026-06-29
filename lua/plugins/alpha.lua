bb1e8bdf6319d0a2f2e0a2e475c03e882372011e
repusemem <enderman2408@gmail.com>
2026-06-29 10:41:01 +0700

    fix alpha.lua errors

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -39,8 +39,8 @@ return {
 
         dashboard.section.buttons.val = {
             dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-            dashboard.button("r", "  Recent", ":Telescope oldfiles <CR>"),
+            dashboard.button("f", "  Find file", ":FzfLua files <CR>"),
+            dashboard.button("r", "  Recent", ":Fzflua oldfiles <CR>"),
             dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
         }
 
